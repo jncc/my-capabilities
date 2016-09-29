15e4f936af1009a5cb2042d40dd17005f69c476e
Pete Montgomery <sumo.the.cat@gmail.com>
2016-09-29 15:16:45 +0100

    first commit

---
+++ src/greeter.ts
@@ -0,0 +1,13 @@
+
+
+export class Greeter {
+  private greeting: string;
+
+  constructor(message: string) {
+    this.greeting = message;
+  }
+
+  public greet() {
+    return "Bonjour, " + this.greeting + "!";
+  }
+};
