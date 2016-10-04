5b8ada78079de75592f8aff6ca3291b4b20a589a
Pete Montgomery <sumo.the.cat@gmail.com>
2016-10-04 12:26:24 +0100

    use express.js

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,18 @@
-import { layers } from "./layers-demo-config";
+// import { layers } from "./layers-demo-config";
 
-console.log(layers);
+import * as express from "express";
+
+const PORT = 8899;
+
+let app = express();
+
+app.get("/", (request, response) => {
+  response.send('It works! Path: ' + request.url);
+});
+
+app.listen(PORT, () => {
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
+
+
+// console.log(layers);
