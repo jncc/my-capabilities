6fb0fce6bb10a44a508f4b6658eea926c8f8a615
Pete Montgomery <sumo.the.cat@gmail.com>
2016-10-05 14:49:27 +0100

    fix tests

--- test/my-test.ts
+++ test/my-test.ts
@@ -1,17 +1,13 @@
-
 /// <reference path="../node_modules/@types/mocha/index.d.ts" />
 
-import * as chai from "chai";
-import { ILayer } from "../src/layer";
-import { makeLayerXml } from "../src/make-response";
-
-const should = chai.should();
+import { expect } from "chai";
+import { makeLayerXml } from "../src/capabilities";
 
 describe("spike", () => {
 
   it("should equal 3", () => {
     let three = 3;
-    three.should.equal(3);
+    expect(three).to.equal(3);
   });
 
 });
@@ -21,13 +17,12 @@ describe("make-layer-xml", () => {
   it("should work", () => {
     let layer = {
       Name: "Some layer",
-      Title: "Some layer title"
+      Title: "Some layer title",
+      Abstract: null
     };
     let result = makeLayerXml(layer);
+    expect(result).to.have.string("<Name>Some layer</Name>")
     console.log(result);
-    //result.should.equal(3);
   });
 
 });
-
-
