fddea139314109f563009ffdbdd4cf2e2e235ed8
Pete Montgomery <sumo.the.cat@gmail.com>
2016-11-01 15:32:49 +0000

    test ordering of express config

--- app.server/layers/layers.ts
+++ app.server/layers/layers.ts
@@ -4,23 +4,28 @@ import * as turf from "turf";
 import { Query } from "../../app.shared/Query"
 import { SCENES } from "./scenes";
 
-export function layers(query: Query): any {
+export function handleLayers(query: Query) {
+
+  // temporary til model binding works
+  query.bbox = [-5, 53, 2.021, 57];
 
   let boundingBox = turf.bboxPolygon(query.bbox);
 
   let results = SCENES
     .filter(s => turf.intersect(s.polygon, boundingBox));
 
-  return results.length;
+  return results;
 }
 
-let query: Query = {
-  bbox: [-5, 53, 2.021, 57],
-  start: new Date(),
-  end: new Date(),
-  type: 'raw'
-};
 
-let result = layers(query);
-console.log(result);
+
+// let query: Query = {
+//   bbox: [-5, 53, 2.021, 57],
+//   start: new Date(),
+//   end: new Date(),
+//   type: 'raw'
+// };
+
+// let result = handleLayers(query);
+// console.log(result);
 
--- app.server/server.ts
+++ app.server/server.ts
@@ -4,24 +4,30 @@ import * as express from "express";
 import { getEnvironmentSettings } from "./settings";
 import { layers } from "./capabilities/demo";
 import { getCapabilities } from "./capabilities/capabilities";
+import { handleLayers } from "./layers/layers";
 
 let app = express();
 let env = getEnvironmentSettings(app.settings.env);
-
-// handle http data requests
-app.get(`/capabilities`, (request, response) => {
-  response.set(`Content-Type`, `text/xml`);
-  let xml = getCapabilities(layers);
-  response.send(xml);
-});
 
 // serve static files from the following directory
 app.use(express.static(env.dir));
 
 // start the express web server
 app.listen(env.port, () => {
-    console.log(`app.server listening on: http://localhost:${env.port}`);
-    console.log(`node environment is ${env.name}`);
+  console.log(`app.server listening on: http://localhost:${env.port}`);
+  console.log(`node environment is ${env.name}`);
 });
 
+// handle http data requests
+
+app.get(`/capabilities`, (req, res) => {
+  let result = getCapabilities(layers);
+  res.set(`Content-Type`, `text/xml`);
+  res.send(result);
+});
+
+app.get(`/layers`, (req, res) => {
+  let result = handleLayers(req.query);
+  res.json(result);
+});
 
