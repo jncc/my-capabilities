af3e5fb4d6d030a8c6c97df5722362a96ae02e19
Pete Montgomery <sumo.the.cat@gmail.com>
2016-11-01 15:38:55 +0000

    reverse experiment

--- app.server/server.ts
+++ app.server/server.ts
@@ -9,15 +9,6 @@ import { handleLayers } from "./layers/layers";
 let app = express();
 let env = getEnvironmentSettings(app.settings.env);
 
-// serve static files from the following directory
-app.use(express.static(env.dir));
-
-// start the express web server
-app.listen(env.port, () => {
-  console.log(`app.server listening on: http://localhost:${env.port}`);
-  console.log(`node environment is ${env.name}`);
-});
-
 // handle http data requests
 
 app.get(`/capabilities`, (req, res) => {
@@ -29,5 +20,14 @@ app.get(`/capabilities`, (req, res) => {
 app.get(`/layers`, (req, res) => {
   let result = handleLayers(req.query);
   res.json(result);
+});
+
+// serve static files from the following directory
+app.use(express.static(env.dir));
+
+// start the express web server
+app.listen(env.port, () => {
+  console.log(`app.server listening on: http://localhost:${env.port}`);
+  console.log(`node environment is ${env.name}`);
 });
 
